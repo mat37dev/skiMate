9c54382277dcc5cde9a85a203c634f6ef11e88d0
Mathieu <mathieu.crosnier15@outlook.fr>
2024-12-05 11:19:30 +0100

    Ajout de la fonction logout. Ajout d'un refresh token et de sa route.

--- SkiMateProject/src/Security/JsonLoginSuccessHandler.php
+++ SkiMateProject/src/Security/JsonLoginSuccessHandler.php
@@ -2,7 +2,9 @@
 
 namespace App\Security;
 
-use App\Repository\UsersRepository;
+use App\Entity\RefreshToken;
+use App\Repository\RefreshTokenRepository;
+use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,18 +13,37 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerI
 
 class JsonLoginSuccessHandler implements AuthenticationSuccessHandlerInterface
 {
-
     private JWTTokenManagerInterface $jwtManager;
-    public function __construct(JWTTokenManagerInterface $jwtManager)
+    private RefreshTokenManagerInterface $refreshTokenManager;
+    private RefreshTokenRepository $refreshTokenRepository;
+
+    public function __construct(JWTTokenManagerInterface $jwtManager, RefreshTokenManagerInterface $refreshTokenManager,
+                                RefreshTokenRepository $refreshTokenRepository)
     {
         $this->jwtManager = $jwtManager;
+        $this->refreshTokenManager = $refreshTokenManager;
+        $this->refreshTokenRepository = $refreshTokenRepository;
     }
+
     public function onAuthenticationSuccess(Request $request, TokenInterface $token): JsonResponse
     {
         $user = $token->getUser();
         $jwt = $this->jwtManager->create($user);
+
+        $existingRefreshToken = $this->refreshTokenRepository->findOneBy(['username' => $user->getUserIdentifier()]);
+        if($existingRefreshToken) {
+            $this->refreshTokenManager->delete($existingRefreshToken);
+        }
+
+        $refreshToken = new RefreshToken();
+        $refreshToken->setRefreshToken(bin2hex(random_bytes(32)));
+        $refreshToken->setUsername($user->getUserIdentifier());
+        $refreshToken->setValid((new \DateTime())->modify(sprintf('+%d seconds', (int) $_ENV['JWT_REFRESH_TOKEN_TTL'])));
+        $this->refreshTokenManager->save($refreshToken);
+
         return new JsonResponse([
             'token' => $jwt,
+            'refresh_token' => $refreshToken->getRefreshToken(),
             'message' => 'Connexion Réussie',
             'user' => [
                 'email' => $user->getUserIdentifier(),
